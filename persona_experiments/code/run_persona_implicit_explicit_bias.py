059700926f0d197006054685e8e2202466ca2a0d
ucabcg3 <clarecgrogan@gmail.com>
2024-08-12 10:36:42 +0100

    results

--- persona_experiments/code/run_persona_implicit_explicit_bias.py
+++ persona_experiments/code/run_persona_implicit_explicit_bias.py
@@ -3,14 +3,14 @@ import argparse
 from persona_implicit_explicit_bias import abuse_implicit_explicit_bias
 
 class PersonaBias:
-    def __init__(self, home_path, bias, iterations):
+    def __init__(self, home_path, bias, iterations, models):
         self.home_path = home_path
         self.bias = bias
         self.df = pd.read_csv(home_path + 'stimuli/{}_iat_stimuli.csv'.format(bias))
         self.domains = {k: [] for k in self.df['category'].unique()}
         for domain in self.domains.keys():
             self.domains[domain] = list(self.df['dataset'][self.df['category'] == domain].unique())
-        self.models = ['llama_2_13b', 'llama_2_7b', 'llama_3_8b']
+        self.models = models
         self.path_name = home_path + "results/persona_{}_iat/".format(bias)
         self.iterations = iterations
 
@@ -25,17 +25,22 @@ class PersonaBias:
                                                  iterations=self.iterations,
                                                  bias=self.bias,
                                                  df=self.df).run_model()
+                    
+
+def list_of_strings(arg):
+    return arg.split(',')
 
 if __name__ == "__main__":
     # Create the parser
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Process some arguments.')
     parser.add_argument('--home_path', type=str, default="/home/ucabcg3/Scratch/msc_bias_llm_project/persona_experiments/", help='Home path')
     parser.add_argument('--bias', type=str, default="abuse", help='Bias')
     parser.add_argument('--iterations', type=int, nargs='+', default=[1, 2, 3], help='Iterations')
+    parser.add_argument('--models', type=list_of_strings, help='Models')
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Create an instance of BiasRunner and run it
-    runner = PersonaBias(args.home_path, args.bias, args.iterations)
+    runner = PersonaBias(args.home_path, args.bias, args.iterations, args.models)
     runner.run()
