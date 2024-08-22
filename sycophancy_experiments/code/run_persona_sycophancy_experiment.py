0a97f9eb5c61ed8ebf0091ffaa2058efd462a622
ucabcg3 <clarecgrogan@gmail.com>
2024-08-22 13:03:32 +0100

    clean up code

--- sycophancy_experiments/code/run_persona_sycophancy_experiment.py
+++ sycophancy_experiments/code/run_persona_sycophancy_experiment.py
@@ -6,7 +6,7 @@ class SycophancyExperiment:
     def __init__(self, home_path, biases, iterations):
         self.home_path = home_path
         self.biases = biases
-        self.models = ['llama_2_7b', 'llama_3_8b', 'llama_2_13b', 'llama_2_70b', 'llama_3_70b']
+        self.models = ['llama_2_7b', 'llama_2_13b', 'llama_3_8b', 'llama_3_70b', 'llama_2_70b']
         self.iterations = iterations
 
     def run(self):
@@ -23,7 +23,7 @@ class SycophancyExperiment:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--home_path', type=str, default="/home/ucabcg3/Scratch/msc_bias_llm_project/sycophancy_experiments/", help='Home path')
-    parser.add_argument('--biases', type=list, default=["abuse", "control"], help='Bias')
+    parser.add_argument('--biases', type=list, default=["control", "abuse"], help='Bias')
     parser.add_argument('--iterations', type=int, nargs='+', default=[1, 2, 3], help='Iterations')
 
     args = parser.parse_args()
